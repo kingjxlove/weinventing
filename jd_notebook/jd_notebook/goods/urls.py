fd909af21f9e8ff69b60ad6d9646365db0f481ab
kingjx <ioen32@163.com>
2019-01-20 16:44:17 +0800

    序列化数据

--- jd_notebook/jd_notebook/goods/urls.py
+++ jd_notebook/jd_notebook/goods/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 
+from rest_framework.routers import SimpleRouter
+
+from jd_notebook.goods import views
+
+router = SimpleRouter()
+router.register(r'^info', views.NotebookView)
 
 urlpatterns = [
 
 ]
+
+urlpatterns += router.urls
