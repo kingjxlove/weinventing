8bf0e77b607fd679c3802a1dd138819fa4e3fa86
kingjx <ioen32@163.com>
2019-01-20 17:44:17 +0800

    数据库迁移以及接口调试

--- jd_notebook/jd_notebook/goods/urls.py
+++ jd_notebook/jd_notebook/goods/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from rest_framework.routers import SimpleRouter
 
-from jd_notebook.goods import views
+from goods import views
 
 router = SimpleRouter()
 router.register(r'^info', views.NotebookView)
